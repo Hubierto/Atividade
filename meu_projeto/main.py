798b271cc6a18d5f2d36382f73d3a73887364221
Roberto <161033388+Hubierto@users.noreply.github.com>
2024-09-18 14:25:58 +0000

    ok

--- meu_projeto/main.py
+++ meu_projeto/main.py
@@ -1,9 +1,6 @@
 import os
 from models.cliente import Cliente
-
 from models.enums.Sexo import Sexo
-
-
 
 os.system("cls||clear")
 cliente1 = Cliente("A", "1", "a", "0", "1", "0", "01", Sexo.MASCULINO, 000)
