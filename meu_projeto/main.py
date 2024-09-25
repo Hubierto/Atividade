d537ca61711170a2cc0ad884a859c5faf8d2a1f2
Roberto <161033388+Hubierto@users.noreply.github.com>
2024-09-25 13:02:22 +0000

    ok

--- meu_projeto/main.py
+++ meu_projeto/main.py
@@ -1,10 +1,13 @@
 import os
-#from models.juridica import Juridica
+from models.juridica import Juridica
 from models.medico import Medico
+from models.enums.Sexo import Sexo
+from models.enums.setor import Setor
+
 
 os.system("cls || clear")
-#juridica1 = Juridica("a", "0", "@", "...", "123", "999")
-medico1 = Medico()
+juridica1 = Juridica("a", "0", "@", "...", "123", "999")
+medico1 = Medico("a","1","a","a","1","a","a",Sexo.FEMININO,"a",Setor.ENGENHARIA, 2.000, "a")
 
-
-#print(juridica1)
+print(medico1)
+print(juridica1)
--- meu_projeto/models/enums/unidadefederativa.py
+++ meu_projeto/models/enums/unidadefederativa.py
@@ -1,7 +1,7 @@
 from enum import Enum
 
 class UnidadeFederativa(Enum):
-    BAHIA = ("Bahia")
+    BAHIA = ("Bahia", "BA")
     SAO_PAULO = ("São Paulo", "SP")
     RIO_DE_JANEIRO = ("Rio de Janeiro", "RJ")
 
@@ -10,6 +10,5 @@ class UnidadeFederativa(Enum):
         self.nome = nome
         self.sigla = sigla
 
-    def __str__(self):
-        return f"{self.nome} | ({self.sigla})"
+
      
